24c24d489f994a68c03f818da0d50ec5adb593e6
Ocean Rescue <Ocean.Rescue@yahoo.com>
2017-06-19 11:41:45 +0300

    Initial Commit

---
+++ VNPermission/VNPermission.h
@@ -0,0 +1,19 @@
+//
+//  VNPermission.h
+//  VNPermission
+//
+//  Created by SubMarine on 19/06/2017.
+//  Copyright © 2017 Mistral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VNPermission.
+FOUNDATION_EXPORT double VNPermissionVersionNumber;
+
+//! Project version string for VNPermission.
+FOUNDATION_EXPORT const unsigned char VNPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VNPermission/PublicHeader.h>
+
+
